b9e24857c7456bc48b39735c2a3a720b8dbf6da9
ChillDiver <yumingchang1991@gmail.com>
2022-06-21 20:18:31 +0800

    feat: init routes

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@ const router = require('express').Router()
 const home = require('./modules/home')
 const users = require('./modules/users')
 const records = require('./modules/records')
+const { authenticator } = require('../middleware/auth')
 
 router.use('/users', users)
-router.use('/records', records)
-router.use('/', home)
+router.use('/records',authenticator , records)
+router.use('/', authenticator, home)
 
 module.exports = router
