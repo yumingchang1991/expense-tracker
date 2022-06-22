9a290d1682e4f462d2f84e57549a6e5d84a2488a
ChillDiver <yumingchang1991@gmail.com>
2022-06-22 19:17:15 +0800

    fix: format date string to YYYY-mm-dd

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const Record = require('../../models/record')
+const formatDateString = require('../../utils/formatDateString')
 
 router.route('/').get((req, res) => {
   Record
@@ -8,10 +9,18 @@ router.route('/').get((req, res) => {
     .lean()
     .sort({ date: 'desc' })
     .then(records => {
+      // calculate total amount
       const totalAmount = records.reduce((sum, record) => {
         return sum += record.amount
       }, 0)
-      res.render('index', { records, totalAmount })
+      
+      // format date string to YYYY-mm-dd
+      const recordsFormatted = records.map(record => {
+        record.date = formatDateString(record.date)
+        return record
+      })
+      
+      res.render('index', { records: recordsFormatted, totalAmount })
     })
     .catch(err => console.log(err))
 })
--- routes/modules/records.js
+++ routes/modules/records.js
@@ -1,17 +1,50 @@
 const router = require('express').Router()
 const Record = require('../../models/record')
 const Category = require('../../models/category')
+const formatDateString = require('../../utils/formatDateString')
 
-router.route('/:id/edit').get((req, res) => {
+
+router.route('/:id/edit').get(async (req, res) => {
   Record
     .findById(req.params.id)
+    .populate('categoryId')
     .lean()
     .then(record => {
       if (!record) {
         return console.log('cannot find record in database. id=', req.params.id)
       }
-      return res.render('editRecord', { record })
+      // format date into YYYY-MM-DD to set default value
+      record.date = formatDateString(record.date)
+
+      // render handlebars template with helper function to decide which category is default
+      return res.render('editRecord',
+        { 
+          record,
+          helpers: {
+            isSelected (category) {
+              return this.record.categoryId.name === category
+            }
+          }
+        }
+      )
     })
+    .catch(err => console.log(err))
+})
+
+router.route('/:id').put((req, res) => {
+  Record
+    .findByIdAndUpdate(
+      req.params.id,
+      { ...req.body }
+    )
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
+})
+
+router.route('/:id').delete((req, res) => {
+  Record
+    .findByIdAndDelete(req.params.id)
+    .then(() => res.redirect('/'))
     .catch(err => console.log(err))
 })
 
