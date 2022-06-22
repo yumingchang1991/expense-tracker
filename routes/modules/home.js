456e07578b9aaa68be731c07797f114fa8c9216b
ChillDiver <yumingchang1991@gmail.com>
2022-06-22 10:23:54 +0800

    feat: make root path dynamically renders records

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,7 +1,19 @@
 const router = require('express').Router()
+const Record = require('../../models/record')
 
 router.route('/').get((req, res) => {
-  res.render('index')
+  Record
+    .find({ userId: req.user._id })
+    .populate('categoryId')
+    .lean()
+    .sort({ date: 'desc' })
+    .then(records => {
+      const totalAmount = records.reduce((sum, record) => {
+        return sum += record.amount
+      }, 0)
+      res.render('index', { records, totalAmount })
+    })
+    .catch(err => console.log(err))
 })
 
 module.exports = router
