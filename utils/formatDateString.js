a0b67b770b860f069d645dbd23715fab7a832c0b
ChillDiver <yumingchang1991@gmail.com>
2022-06-22 19:16:16 +0800

    feat: add formatDateString

---
+++ utils/formatDateString.js
@@ -0,0 +1,7 @@
+module.exports = function formatDateString(date) {
+  return [
+    date.getFullYear().toString().padStart(4, '0'),
+    (date.getMonth() + 1).toString().padStart(2, '0'),
+    date.getDate().toString().padStart(2, '0')
+  ].join('-')
+}
