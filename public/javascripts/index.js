29cae7c4adc64470315dbff69f2ccab7459739ce
ChillDiver <yumingchang1991@gmail.com>
2022-06-24 17:40:59 +0800

    feat: add a confirm msg before removing expense

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,18 @@
 const filterForm = document.querySelector('#filter-form')
 const Select = filterForm.querySelector('select')
 
+const expenseTable = document.querySelector('table')
+
 Select.addEventListener('change', e => {
   e.preventDefault()
   filterForm.submit()
+})
+
+expenseTable.addEventListener('click', e => {
+  e.preventDefault()
+  if (e.target.classList.contains('button-delete-expense')) {
+    if (window.confirm("please confirm you'd like to remove this expense")) {
+      return e.target.parentElement.submit()
+    }
+  }
 })
