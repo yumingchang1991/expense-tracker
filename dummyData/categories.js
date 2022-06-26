6c089847ceb77bfab3ca02255001c4da5db0f008
ChillDiver <yumingchang1991@gmail.com>
2022-06-26 17:27:17 +0800

    feat: make font size and icon smaller

--- dummyData/categories.js
+++ dummyData/categories.js
@@ -1,8 +1,8 @@
 module.exports = [
-  { name: 'Utilities', iconClass: 'fa-2xl fa-solid fa-house' },
-  { name: 'Groceries', iconClass: 'fa-2xl fa-solid fa-basket-shopping' },
-  { name: 'Transportation', iconClass: 'fa-2xl fa-solid fa-van-shuttle' },
-  { name: 'Entertainment', iconClass: 'fa-2xl fa-solid fa-face-grin-beam' },
-  { name: 'Food', iconClass: 'fa-2xl fa-solid fa-utensils' },
-  { name: 'Others', iconClass: 'fa-2xl fa-solid fa-pen' }
+  { name: 'Utilities', iconClass: 'fa-xl fa-solid fa-house' },
+  { name: 'Groceries', iconClass: 'fa-xl fa-solid fa-basket-shopping' },
+  { name: 'Transportation', iconClass: 'fa-xl fa-solid fa-van-shuttle' },
+  { name: 'Entertainment', iconClass: 'fa-xl fa-solid fa-face-grin-beam' },
+  { name: 'Food', iconClass: 'fa-xl fa-solid fa-utensils' },
+  { name: 'Others', iconClass: 'fa-xl fa-solid fa-pen' }
 ]
